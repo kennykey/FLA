742e6b1a557d6d79d291eef61e7890785fcff450
Alfred Tambayong <alfredt44@yahoo.com>
2021-11-01 18:49:14 +0700

    perubahan terakhir gw

--- FLA/src/Main.java
+++ FLA/src/Main.java
@@ -25,6 +25,7 @@ public class Main {
 		String name="";
 		String size="";
 		String payment="";
+		String accountNumber;
 		double nomor=0;
 		double nomor2=0;
 		
@@ -32,8 +33,6 @@ public class Main {
 			System.out.println("Outfit Type [Dress | Shoes]:");
 			Type=scan.nextLine();
 		}while(!Type.equals("Dress") && !Type.equals("Shoes"));
-		
-		if(!Type.equals("Dress")) {
 
 			do {
 				System.out.println("Outfit Category [Artys | Classic]:");
@@ -50,44 +49,31 @@ public class Main {
 				nomor=scan.nextDouble(); scan.nextLine();
 			}while(nomor > 1000000 || nomor < 10000);
 			
-			do {
-				System.out.println("Input Size [Small | Medium | Large]:");
-				size=scan.nextLine();
-			}while(!size.equals("Small") && !size.equals("Medium") && !size.equals("Large"));
+			if (Type.equals("Dress")) {
+				do {
+					System.out.println("Input Size [Small | Medium | Large]:");
+					size=scan.nextLine();
+				}while(!size.equals("Small") && !size.equals("Medium") && !size.equals("Large"));
+			} else {
+				int sizeShoes;
+				do {
+					System.out.println("Input Size [34 - 46]:");
+					sizeShoes=scan.nextInt();
+					size = Integer.toString(sizeShoes);
+				}while(sizeShoes <34 && sizeShoes >36);
+			}
 			
 			do {
 				System.out.println("Input Payment [Credit | Transfer]:");
 				payment=scan.nextLine();
+				if (payment.equals("Transfer")) {
+					do {
+						System.out.println("Input account number [10 chars]: ");
+						accountNumber = scan.nextLine();
+					}while(!accountNumber.length());
+					
+				}
 			}while(!payment.equals("Credit") && !payment.equals("Transfer"));
-			
-		}else if(!Type.equals("Shoes")){
-			
-			do {
-				System.out.println("Outfit Category [Artys | Classic]:");
-				Category=scan.nextLine();
-			}while(!Category.equals("Artys") && !Category.equals("Classic"));
-			
-			do {
-				System.out.println("Input Name [3-20 chars]:");
-				name=scan.nextLine();
-			}while(name == null);
-			
-			do {
-				System.out.println("Input Price [10000 - 1000000]:");
-				nomor=scan.nextDouble(); scan.nextLine();
-			}while(nomor > 1000000 || nomor < 10000);
-			
-			do {
-				System.out.println("Input Size [34 - 46]:");
-				nomor2=scan.nextDouble(); scan.nextLine();
-			}while(nomor2 > 46 || nomor2 < 34);
-			
-			do {
-				System.out.println("Input Payment [Credit | Transfer]:");
-				payment=scan.nextLine();
-			}while(!payment.equals("Credit") && !payment.equals("Transfer"));
-			
-		}
 	}
 	
 	public void menu2() {
